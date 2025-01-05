ded52deb83a9ff93583ee76ee40d4ade57bb7a51
Mimito164 <fjaralampidis@gmail.com>
2025-01-05 01:50:18 -0300

    agrego interaccion simple con la db

---
+++ Front/src/components/algohttp.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// import { useCounterStore } from '@/stores/counter';
+// import { storeToRefs } from 'pinia'
+import axios, { type AxiosResponse } from 'axios';
+import { ref } from 'vue';
+
+const res = ref()
+
+// const store = useCounterStore()
+// const {count} = storeToRefs(store)
+// const {increment} = store
+
+
+async function fetchear() {
+    const response = await axios.get("http://127.0.0.1:8000/api/")
+    res.value = response
+}
+
+fetchear()
+
+</script>
+
+<template>
+    <div><button @click="fetchear">fetchear</button></div>
+    <div v-if="res!=null">
+        {{ res }}
+    </div>
+</template>
